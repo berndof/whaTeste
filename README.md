42234ff23504f00cbd2528345c46e4a3c056a060
berndof <bernasoestudas@gmail.com>
2023-05-04 10:16:43 -0300

    primeiroTeste.2

---
+++ README.md
@@ -0,0 +1,23 @@
+- Antes de começar, certifique-se de que seu sistema esteja atualizado executando o seguinte comando:
+
+```bash
+sudo apt update && sudo apt upgrade -y
+```
+
+- Certifique-se também de ter o Docker e o docker-compose instalados em seu sistema. Você pode instalá-los executando o seguinte comando:
+
+```bash
+sudo apt install docker.io docker-compose -y
+```
+
+- Clone o repositório do GitHub para seu sistema:
+
+```bash
+git clone https://github.com/seu-usuario/nome-do-repositorio.git
+```
+
+- Navegue até o diretório onde o script de instalação install.sh está localizado:
+
+```bash 
+cd nome-do-repositorio
+```
--- install.sh
+++ install.sh
@@ -13,11 +13,17 @@ export MYSQL_PASSWORD=$MYSQL_PASSWORD
 export DB_PORT=$DB_PORT
 export PHPMYADMIN_PORT=$PHPMYADMIN_PORT
 
+chmod +x install.sh
+
 docker-compose up -d
 
-echo "MySQL root password: $MYSQL_ROOT_PASSWORD"
-echo "MySQL database name: $MYSQL_DATABASE"
-echo "MySQL user name: $MYSQL_USER"
-echo "MySQL user password: $MYSQL_PASSWORD"
-echo "Database port: ${DB_PORT:-3306}"
-echo "PHPMyAdmin port: ${PHPMYADMIN_PORT:-8080}"
+printf "\n\nAs seguintes configurações foram definidas:\n"
+printf "--------------------------------------------\n"
+printf "  MySQL root password: %s\n" "$MYSQL_ROOT_PASSWORD"
+printf "  MySQL database name: %s\n" "$MYSQL_DATABASE"
+printf "  MySQL user name: %s\n" "$MYSQL_USER"
+printf "  MySQL user password: %s\n" "$MYSQL_PASSWORD"
+printf "  Database port: %s\n" "${DB_PORT:-3306}"
+printf "  PHPMyAdmin port: %s\n" "${PHPMYADMIN_PORT:-8080}"
+
+docker ps
